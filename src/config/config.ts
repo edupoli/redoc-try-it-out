5eb4244a0f942b3d7baf144eed0448b642cb31f2
Eduardo Policarpo <eduardo@eduardos-ipro.poli.local>
2024-11-29 16:59:16 -0300

    fix: alterado tsconfig.jsong

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,19 @@
-export abstract class Config<T> {
-    protected readonly options:T;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export abstract class Config<T extends object> {
+  protected readonly options: T;
 
-    public constructor(options: T) {
-        this.options = options;
-        for ( const key of Object.keys(options) as Array<keyof Config<T>> ) {
-            const option = (key in this ? key : `_${key}` as keyof Config<T>);
-            if ( option in this ) {
-                this[option] = options[key];
-            }
-        }
-    }
+  public constructor(options: T) {
+    this.options = options;
 
-    protected get cdnUrl(): string {
-        return 'https://cdn.jsdelivr.net/npm/';
+    for (const key of Object.keys(options) as Array<keyof T>) {
+      const option = key in this ? key : (`_${String(key)}` as keyof this);
+      if (option in this) {
+        (this as any)[option] = options[key];
+      }
     }
+  }
+
+  protected get cdnUrl(): string {
+    return "https://cdn.jsdelivr.net/npm/";
+  }
 }
